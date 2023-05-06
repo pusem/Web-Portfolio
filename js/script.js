552380eadd235e027c26f3af9e5a0f9470d1cb5a
Pusetso Mguzulwa <92914430+pusetsom@users.noreply.github.com>
2023-05-06 22:14:45 +0200

    Updated the code to hide menu after a click
    The menu will automatically close on smaller devices after the navigation link is clicked.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
 // header scrolling effect
 $(window).on('scroll', function(){
 	if($(window).scrollTop()){
-      $('header').addClass('nav-show');
-		  
-	} 
-	else{
+		$('header').addClass('nav-show');
+	} else{
 		$('header').removeClass('nav-show');
 	}
-	   
 })
 
-//hamburger
+// hamburger
 const navSlide = () => {
-	 const hamburger = document.querySelector(".hamburger");
-	 const navbar = document.querySelector(".nav-bar");
-	 const navLinks = document.querySelectorAll(".nav-bar li");
+	const hamburger = document.querySelector(".hamburger");
+	const navbar = document.querySelector(".nav-bar");
+	const navLinks = document.querySelectorAll(".nav-bar li");
 
-     hamburger.onclick = () => {
-		
-	 navbar.classList.toggle("nav-active");
-		 
-      //Animation links
-	 navLinks.forEach((link, index) => {
-		if (link.style.animation) {
-			link.style.animation = "";
-		} else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
-		   }
+	hamburger.onclick = () => {
+		navbar.classList.toggle("nav-active");
+
+		// Animation links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+			}
 		});
-	  //hamburger animation
-	 hamburger.classList.toggle("toggle");
-    }
-	 
+
+		// hamburger animation
+		hamburger.classList.toggle("toggle");
 	}
+
+	// Close menu when a navigation link is clicked
+	navLinks.forEach((link) => {
+		link.addEventListener('click', () => {
+			navbar.classList.remove("nav-active");
+			hamburger.classList.remove("toggle");
+		});
+	});
+}
 
 window.onload = () => navSlide();
